720c318baac6b740463786f90e0e74c5ca2f084a
Julianna Stevenson <steve146@purdue.edu>
2018-06-02 21:48:28 -0700

    generate search frequency per hour

--- browser_history.py
+++ browser_history.py
@@ -1,5 +1,34 @@
 import json
 import sys
+import time
+
+"""
+Generate a dictionary describing search frequency per hour.
+
+Args:
+    browser_history: List of searches
+Returns:
+    search_frequency_per_hour: Dict of hour (military time) to search frequency
+"""
+def GenerateSearchFrequencyPerHour(browser_history):
+    hour_totals = [0] * 24
+    search_frequency_per_hour = [0] * 24
+    total_searches = 0.0
+
+    for search in browser_history:
+        total_searches += 1
+        usec_time = search['time_usec']
+        hour = int(time.strftime("%H", time.gmtime(usec_time)))
+        hour_totals[hour] += 1
+
+
+    for i in range(len(search_frequency_per_hour)):
+        search_frequency_per_hour[i] = hour_totals[i]/total_searches
+
+    return search_frequency_per_hour
+
+
+
 
 if __name__ == '__main__':
     if (len(sys.argv) < 2):
@@ -10,6 +39,7 @@ if __name__ == '__main__':
 
         browser_history = data['Browser History']
 
-        // TODO: Generate graph of average time spent searching during day
+        # TODO: Generate graph of average time spent searching during day
+        GenerateSearchFrequencyPerHour(browser_history)
 
-        // TODO: Generate graph of most frequent websites
+        # TODO: Generate graph of most frequent websites
